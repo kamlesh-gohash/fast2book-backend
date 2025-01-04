b1fe92f96f2a45b1d5edc8f642264f7520436375
kivi992882 <vikram.singh@gohashinclude.in>
2025-01-04 12:37:05 +0530

    reslove test issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,18 @@
-from collections.abc import Callable, Generator
-from typing import Any
+# from collections.abc import Callable, Generator
+# from typing import Any
 
-import pytest
+# import pytest
 
-from fastapi.testclient import TestClient
+# from fastapi.testclient import TestClient
 
-from app.main import app
-
-
-@pytest.fixture
-def client() -> Generator[TestClient, Any, None]:
-    yield TestClient(app)
-    app.dependency_overrides = {}
+# from app.main import app
 
 
-def override_dependency(dependency: Callable[..., Any], mocked_response: Any) -> None:
-    app.dependency_overrides[dependency] = lambda: mocked_response
+# @pytest.fixture
+# def client() -> Generator[TestClient, Any, None]:
+#     yield TestClient(app)
+#     app.dependency_overrides = {}
+
+
+# def override_dependency(dependency: Callable[..., Any], mocked_response: Any) -> None:
+#     app.dependency_overrides[dependency] = lambda: mocked_response
