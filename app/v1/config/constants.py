b3f03d94ce3027d5a395d10d3b0dd16890d35299
prashant-gohash <prashant@gohashinclude.com>
2025-05-23 17:08:37 +0530

    feat: add apple login

--- app/v1/config/constants.py
+++ app/v1/config/constants.py
@@ -15,6 +15,11 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 FRONT_URL = os.getenv("FRONT_URL", "http://*")
 
+# Apple Sign In Configuration
+APPLE_CLIENT_ID = os.getenv("APPLE_CLIENT_ID")
+APPLE_TEAM_ID = os.getenv("APPLE_TEAM_ID")
+APPLE_KEY_ID = os.getenv("APPLE_KEY_ID")
+
 # Export all constants as a dictionary for other modules if needed
 ALL_CONSTANTS = {
     "DATABASE_URL": DATABASE_URL,
@@ -24,4 +29,7 @@ ALL_CONSTANTS = {
     "EMAIL_HOST": EMAIL_HOST,
     "EMAIL_PORT": EMAIL_PORT,
     "FRONT_URL": FRONT_URL,
+    "APPLE_CLIENT_ID": APPLE_CLIENT_ID,
+    "APPLE_TEAM_ID": APPLE_TEAM_ID,
+    "APPLE_KEY_ID": APPLE_KEY_ID,
 }
